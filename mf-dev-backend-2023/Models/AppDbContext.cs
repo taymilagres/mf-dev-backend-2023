6b9dfe22e74df2903cc0b7cca7af9cb552f1a1c2
Tayane Milagres <devtaay@gmail.com>
2023-10-11 13:15:53 -0300

    Configuração do Banco de Dados e da Tabela Veiculo

---
+++ mf-dev-backend-2023/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2023.Models
+{
+    public class AppDbContext : DbContext //Essa classe tem a função de configurar o Entity Framework e manipulação de dados
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
--- mf-dev-backend-2023/Program.cs
+++ mf-dev-backend-2023/Program.cs
@@ -1,8 +1,14 @@
+using mf_dev_backend_2023.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();  
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
